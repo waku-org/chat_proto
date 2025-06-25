533bd31357eb76ac0070da1120cef94430bd1361
Jazz Turner-Baggs <473256+jazzz@users.noreply.github.com>
2025-06-25 15:58:49 -0700

    added logging to build

--- rust/umbra-types/build.rs
+++ rust/umbra-types/build.rs
@@ -4,21 +4,27 @@ use std::env;
 use std::path::PathBuf;
 
 fn get_proto_dir() -> PathBuf {
+    println!(
+        "Manifest dir: {:?}",
+        env::var("CARGO_MANIFEST_DIR").unwrap()
+    );
     // protos are stored in ../../proto/
     let mut proto_root_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
     proto_root_dir.pop();
     proto_root_dir.pop();
+
+    println!("proto_dir: {:?}", proto_root_dir);
     proto_root_dir
 }
 
 fn main() {
     let mut config = prost_build::Config::new();
 
-    // Create mod file to maintain import hierarchy 
+    // Create mod file to maintain import hierarchy
     config.include_file("mod.rs");
 
-
-    config.compile_protos(
+    config
+        .compile_protos(
             &[
                 "proto/umbra/base.proto",
                 "proto/umbra/common_frames.proto",
