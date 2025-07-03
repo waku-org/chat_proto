54ebf403202651afdbfd719f25a34f3429c5b680
Jazz Turner-Baggs <473256+jazzz@users.noreply.github.com>
2025-07-02 20:10:34 -0700

    update tests

--- nim/tests/test_encoding.nim
+++ nim/tests/test_encoding.nim
@@ -1,10 +1,10 @@
 import unittest
 
-import chat_types
+import chat_proto
 
 suite "Type Encoding Tests":
   test "HistoryEntry roundtrip": 
-    let x = chat_types.HistoryEntry(message_id: "12345", retrieval_hint: @[1'u8, 2, 3, 255])
+    let x = chat_proto.HistoryEntry(message_id: "12345", retrieval_hint: @[1'u8, 2, 3, 255])
     let encoded = Protobuf.encode(x)
     let decoded = Protobuf.decode(encoded, HistoryEntry)
 
