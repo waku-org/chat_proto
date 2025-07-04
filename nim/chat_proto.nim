7b6c81f1bfaf6148b680145c976705383cf15729
Jazz Turner-Baggs <473256+jazzz@users.noreply.github.com>
2025-07-04 13:32:59 -0700

    updated nim types

--- nim/chat_proto.nim
+++ nim/chat_proto.nim
@@ -1,10 +1,19 @@
 import protobuf_serialization
 import protobuf_serialization/proto_parser
 
-import_proto3 "./proto/base.proto"
+import_proto3 "../proto/umbra/encryption.proto"
+import_proto3 "../proto/umbra/envelope.proto"
+import_proto3 "../proto/umbra/inbox.proto"
+import_proto3 "../proto/umbra/reliability.proto"
 
 export protobuf_serialization
 
 # TODO: Do the Objects have to be listed manually?
+export EncryptedPayload
 export HistoryEntry
+export InboxV1Frame
+export ReliablePayload
+export UmbraEnvelopeV1
+
+
 
--- nim/chat_proto.nimble
+++ nim/chat_proto.nimble
@@ -4,10 +4,11 @@ version       = "0.1.0"
 author        = "jazzz"
 description   = "Type definitions for the chat protocol"
 license       = "MIT"
-bin = @["chat_proto"]
+# bin = @["chat_proto"]
+# installDirs = @["../proto"]
+
 
 # Dependencies
 requires "nim >= 2.0.14"
 requires "chronicles"
 requires "protobuf_serialization"
-requires "protobuf"
